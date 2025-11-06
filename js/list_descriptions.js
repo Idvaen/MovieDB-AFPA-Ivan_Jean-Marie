a8476380a8f2b320850a5c2b4dc735f39c771716
Ivan Denysov <ivan.denysov.v@gmail.com>
2025-11-06 08:24:12 +0100

    Refactor JavaScript files and update HTML to improve movie description functionality

--- js/list_descriptions.js
+++ js/list_descriptions.js
@@ -1,11 +1,8 @@
 "use strict";
 
-let URL = "data/data.json";
-// let affiche_table = document.getElementById("affiche_table");
-
 //Partie description pour chaque film
 if (window.location.href.indexOf("descriptions.html") !== -1) {
-  fetch(URL)
+  fetch("data/data.json")
     .then((response) => response.json())
     .then(function (data) {
       console.log("La requête GET a abouti avec la réponse JSON : ", data);
@@ -18,10 +15,10 @@ if (window.location.href.indexOf("descriptions.html") !== -1) {
         let col = document.createElement("div");
         col.className = "col-12";
         col.innerHTML = `
-        <div class="movie-description">
+        <div class="movie-description" data-movie-id="${data.results[i].id}">
           <h2 class="titre_de_listes">Description de ${data.results[i].title}</h2>
-          <a href="description.html"><img src="https://media.themoviedb.org/t/p/w440_and_h660_face/${data.results[i].poster_path
-          }" class="img-fluid pourfloat size_poster" alt="${data.results[i].title}"></a>
+          <img src="https://media.themoviedb.org/t/p/w440_and_h660_face/${data.results[i].poster_path
+          }" class="img-fluid pourfloat size_poster movie-img" alt="${data.results[i].title}" data-movie-id="${data.results[i].id}">
           <p class="affiche_et_acteur" style="background-color: white;"><strong>Genres:</strong> ${data.results[i].genre_ids.join(", ")}
           <br> <strong>Synopsis:</strong> ${data.results[i].overview}
           <br><strong>Date de sortie:</strong> ${new Date(
@@ -30,30 +27,30 @@ if (window.location.href.indexOf("descriptions.html") !== -1) {
           <br><strong>Note:</strong> ${data.results[i].vote_average} / 10</p>
         </div>
       `;
-      document.querySelector(".description-section .row").appendChild(col);
-      // If you want these description items to open the single description page when clicked,
-      // navigate with the movie id in the query string (same pattern as above).
-      const img = col.querySelector(".movie-img");
-      if (img) {
-        img.addEventListener("click", function (e) {
-          // Prevent any default behaviour (if any)
-          e.preventDefault();
-          const movieId = this.dataset.movieId;
-          // Small delay to allow CSS feedback or prevent accidental double-clicks
-          setTimeout(() => {
-            window.location.href = `description.html?id=${movieId}`;
-          }, 500);
+        document.querySelector(".description-section .row").appendChild(col);
+        // If you want these description items to open the single description page when clicked,
+        // navigate with the movie id in the query string (same pattern as above).
+        const img = col.querySelector(".movie-img");
+        if (img) {
+          img.addEventListener("click", function (e) {
+            // Prevent any default behaviour (if any)
+            e.preventDefault();
+            const movieId = this.dataset.movieId;
+            // Small delay to allow CSS feedback or prevent accidental double-clicks
+            setTimeout(() => {
+              window.location.href = `description.html?id=${movieId}`;
+            }, 500);
 
-          // setTimeout(() => {
-          //   window.location.href = `description.html`;
-          // }, 500);
-        });
+            // setTimeout(() => {
+            //   window.location.href = `description.html`;
+            // }, 500);
+          });
+        }
       }
-    }
-  })
-  .catch(function (error) {
-    console.log("La requête GET a échoué : ", error);
-  });
+    })
+    .catch(function (error) {
+      console.log("La requête GET a échoué : ", error);
+    });
 }
 
 // // If we are on the single description page, auto-run the renderer
