591ae927109b266c103992cd81d2dbb3f2b751d6
Ivan Denysov <ivan.denysov.v@gmail.com>
2025-11-07 10:35:06 +0100

    Refactor film data fetching and display logic in search functionality

--- js/search.js
+++ js/search.js
@@ -6,7 +6,17 @@ rechercheInput.addEventListener("input", rechercherFilms);
 //Search function
 function rechercherFilms() {
     //Function de recherche des films avec API
-    const url = `https://api.themoviedb.org/3/search/movie?query=${rechercheInput.value}&include_adult=true&language=en-US`;
+    if (!rechercheInput.value) {
+        console.log("Aucun terme de recherche.");
+        document.getElementById("searchResults").innerHTML = "";
+        afficherFilmsData();
+        return;
+    }
+
+    document.getElementById("searchResults").innerHTML = "";
+
+    console.log("Recherche en cours...");
+    const url = `https://api.themoviedb.org/3/search/movie?query=${rechercheInput.value}&include_adult=false&language=en-US&page=1`;
     const options = {
         method: 'GET',
         headers: {
@@ -15,50 +25,26 @@ function rechercherFilms() {
         }
     };
 
-    const input = document.getElementById("searchInput").value.toLowerCase();
-    const films = document.querySelectorAll(".movie-card");
-    films.forEach(film => {
-        const title = film.querySelector("h2").textContent.toLowerCase();
-        if (title.includes(input)) {
-            film.style.display = "block";
-            // Call API pour chercher des films
-            fetch(url, options)
-                .then(response => response.json())
-                .then(data => {
-                    // Process the API response
-                    console.log("API Response:", data);
-                    // Afficher les résultats de la recherche
-                    afficherResultatsRecherche(data.results);
-
-                })
-                .catch(error => {
-                    console.error("Error fetching movie details:", error);
-                });
-        } else {
-            film.style.display = "none";
-        }
-    });
-}
-
-function afficherResultatsRecherche(films) {
-    const resultsContainer = document.getElementById("searchResults");
-    resultsContainer.innerHTML = ""; // Clear previous results
-
-    if (films.length === 0) {
-        resultsContainer.innerHTML = "<p>Aucun résultat trouvé.</p>";
-        return;
-    }
-
-    films.forEach(film => {
-        const filmElement = document.createElement("div");
-        filmElement.classList.add("col-6");
-        filmElement.innerHTML = `
-        <div class="movie-info movie-description col-12 row">
-            <h2 class="titre_de_listes">${film.title}</h2>
-            <img class="img-fluid pourfloat size_poster movie-img" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="${film.title}" />
-            <p>${film.release_date}</p>
-        </div>
-        `;
-        resultsContainer.appendChild(filmElement);
-    });
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            // Process the API response
+            for (let i in data.results) {
+                console.log("Film trouvé:", data.results[i].title);
+                // Afficher les résultats de la recherche
+                const resultsContainer = document.getElementById("searchResults");
+                resultsContainer.innerHTML += `
+                    <div class="col-6">
+                        <div class="movie-info movie-description col-12 row">
+                            <h2 class="titre_de_listes">${data.results[i].title}</h2>
+                            <img class="img-fluid pourfloat size_poster movie-img" src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" alt="${data.results[i].title}" />
+                            <p>${data.results[i].release_date}</p>
+                        </div>
+                    </div>
+                `;
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching movie details:", error);
+        });
 }
