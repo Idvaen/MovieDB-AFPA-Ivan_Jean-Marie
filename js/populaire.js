51790096b85c4f11c36f14286b29f88449308bac
JMGassies_Portable <jeanmarie.gassies@gmail.com>
2025-11-10 11:48:26 +0100

    lundi modif popuplaire ajouter css et soucis avec affichage resultat recherche

--- js/populaire.js
+++ js/populaire.js
@@ -25,10 +25,10 @@ fetch(url, options)
             let col = document.createElement("div");                 // situé dans la page populaire.html
             col.className = "col-12";           // code Html à insérer dans la page populaire
             col.innerHTML = `                                       
-                <div class="movie-description"><br> 
+                <div class="movie-description" data-movie-id="${films.results[i].id}"><br> 
                 <h2 class="titre_de_listes">${films.results[i].title}</h2>      
-                    <img class="img-fluid pourfloat size_poster" src="https://media.themoviedb.org/t/p/w440_and_h660_face/${films.results[i].poster_path
-                }" alt="${films.results[i].title}"/>
+                    <img class="img-fluid pourfloat size_poster movie-img" src="https://media.themoviedb.org/t/p/w440_and_h660_face/${films.results[i].poster_path
+                }" alt="${films.results[i].title}" data-movie-id="${films.results[i].id}"/>
                     <p class="synopsis"><strong>Synopsis:</strong> ${films.results[i].overview}
                     <br><strong>Date de sortie:</strong> ${new Date(
                     films.results[i].release_date
